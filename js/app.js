e5d85616df6d5af22acfcb7c5b9180504d3cd55a
John Kolden <john.kolden@gmail.com>
2018-04-17 14:07:38 -0700

    added search functionality

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 // Class to represent a row in the venue grid - this prbably isn't needed since our list won't be editable?
-function venue(name) {
-  var self = this;
-  self.name = name;
-}
+var item = function(data) {
+  this.title = data.title;
+  this.location = data.location;
+  this.marker = data.marker;
+};
 
 var map;
+
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.7749, lng: -122.4194},
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 37.7749,
+      lng: -122.4194
+    },
     zoom: 13
   });
 };
@@ -37,82 +42,100 @@ var ViewModel = function() {
 
   self.loadUserData = function() {
 
-  //good tututorial on loading server-side data here: https://code.tutsplus.com/tutorials/accessing-external-data--net-31306
-  $.getJSON("http://api.songkick.com/api/3.0/metro_areas/26330/calendar.json?apikey=Co6kY8qQPER2gGwi", function(data) {
-    self.items(data.resultsPage.results.event);
+    //good tututorial on loading server-side data here: https://code.tutsplus.com/tutorials/accessing-external-data--net-31306
+    $.getJSON("http://api.songkick.com/api/3.0/metro_areas/26330/calendar.json?apikey=Co6kY8qQPER2gGwi", function(data) {
+      self.items(data.resultsPage.results.event);
 
-    // The following group uses the location array to create an array of markers on initialize.
-    var markers = [];
 
-    for (var i = 0; i < self.items().length; i++) {
-      // Get the position from the location array.
-      var position = {"lat": self.items()[i].venue.lat, "lng": self.items()[i].venue.lng};
-      var title = self.items()[i].venue.displayName;
 
-      // Create a marker per location, and put into markers array.
-      var contentString = '<div>This is some data '+ title  + '</div>';
-      var largeInfowindow = new google.maps.InfoWindow({
-      });
+      // The following group uses the location array to create an array of markers on initialize.
+      var markers = [];
 
-//same issue as this guy:
-//https://stackoverflow.com/questions/19784953/google-map-api-infowindow-in-foreach-loop
-//https://stackoverflow.com/questions/32798480/assign-infowindow-for-each-marker-in-google-maps
+      for (var i = 0; i < self.items().length; i++) {
+        // Get the position from the location array.
 
-      var marker = new google.maps.Marker({
-        map: map,
-        position: position,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        id: i
-      });
-
-      //same issue as this guy:
-      //https://stackoverflow.com/questions/32798480/assign-infowindow-for-each-marker-in-google-maps
-
-       google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          self.performances(self.items()[i].performance);
-          var contentString = '<h3>' + self.items()[i].venue.displayName + '</h3>';
-          contentString += '<h4>' + self.items()[i].displayName + '</h4>'
-          for (var j = 0; j <  self.performances().length; j++) {
-          contentString += '<div> Artist ' + (j+1) + ': ' + '<a href=' + self.performances()[j].artist.uri + ' target="_Blank">' + self.performances()[j].displayName + '</a></div>';
+        var position = {
+          "lat": self.items()[i].venue.lat,
+          "lng": self.items()[i].venue.lng
         };
-        contentString += '<br><img src="./img/powered-by-songkick-pink.png" width="100">';
+        var title = self.items()[i].venue.displayName;
+
+        // Create a marker per location, and put into markers array.
+        var contentString = '<div>This is some data ' + title + '</div>';
+        var largeInfowindow = new google.maps.InfoWindow({});
+
+        //same issue as this guy:
+        //https://stackoverflow.com/questions/19784953/google-map-api-infowindow-in-foreach-loop
+        //https://stackoverflow.com/questions/32798480/assign-infowindow-for-each-marker-in-google-maps
+
+        var marker = new google.maps.Marker({
+          map: map,
+          position: position,
+          title: title,
+          animation: google.maps.Animation.DROP,
+          id: i
+        });
+
+        //same issue as this guy:
+        //https://stackoverflow.com/questions/32798480/assign-infowindow-for-each-marker-in-google-maps
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+            //update the performance array with all performances at the venue:
+            self.performances(self.items()[i].performance);
+
+            //build the content string that will be used for the infoWindow:
+            var contentString = '<h3>' + self.items()[i].venue.displayName + '</h3>';
+            contentString += '<h4>' + self.items()[i].displayName + '</h4>';
+
+            //some venues have multiple artists/performances per night, so we loop over the performance array to display them all:
+            for (var j = 0; j < self.performances().length; j++) {
+
+            //link to the SongKick site is required by the SongKick API Terms of Use:
+              contentString += '<div> Artist ' + (j + 1) + ': ' + '<a href=' + self.performances()[j].artist.uri + ' target="_Blank">' + self.performances()[j].displayName + '</a></div>';
+            };
+
+            //image is required by the SongKick API Terms of Use:
+            contentString += '<br><img src="./img/powered-by-songkick-pink.png" width="100">';
+
             largeInfowindow.setContent(contentString);
             largeInfowindow.open(map, marker);
-        }
-    })(marker, i));
+          }
+        })(marker, i));
 
-      // Push the marker to our array of markers.
-      markers.push(marker);
+        //add the markers to the marker array:
+        markers.push(marker);
+        var bounds = new google.maps.LatLngBounds();
 
+        bounds.extend(markers[i].position);
+      }
+      // Extend the boundaries of the map for each marker
+      map.fitBounds(bounds);
 
-
-      //bounds.extend(markers[i].position);
-    }
-    // Extend the boundaries of the map for each marker
-    //map.fitBounds(bounds);
-
-  });
-};
-
+    });
+  };
 
   self.filter = ko.observable('');
 
+  var stringStartsWith = function(string, startsWith) {
+    string = string || "";
+    if (startsWith.length > string.length)
+      return false;
+    return string.substring(0, startsWith.length) === startsWith;
+  };
 
-/*
-  //I was on the wrong track with this so I used this as a reference:
-  //https://stackoverflow.com/questions/31188583/filter-table-contents
+  //https://stackoverflow.com/questions/45461834/filtering-table-using-knockout-js
   self.filteredItems = ko.computed(function() {
-    var filter = self.filter();
+    var filter = self.filter().toLowerCase();
     if (!filter) {
       return self.items();
+    } else {
+      return ko.utils.arrayFilter(self.items(), function(item) {
+        return stringStartsWith(item.venue.displayName.toLowerCase(), filter) ||
+          stringStartsWith(item.performance[0].artist.displayName.toLowerCase(), filter);
+      });
     }
-    return self.items().filter(function(i) {
-      return i.indexOf(filter) > -1;
-    });
-  });
-  */
+  }, this);
+
 };
 
 
