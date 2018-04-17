78ee907c6cec25a48375661f4fe6c93c93128edc
John Kolden <john.kolden@gmail.com>
2018-04-16 22:09:04 -0700

    added infoWindow with songkick API logo

--- js/app.js
+++ js/app.js
@@ -7,16 +7,33 @@ function venue(name) {
 var map;
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
-   map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.7749, lng: -122.4194},
     zoom: 13
   });
 };
 
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    //infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
+    });
+  }
+}
+
 var ViewModel = function() {
 
   var self = this;
   self.items = ko.observableArray([]);
+  self.performances = ko.observableArray([]);
 
   self.loadUserData = function() {
 
@@ -31,7 +48,16 @@ var ViewModel = function() {
       // Get the position from the location array.
       var position = {"lat": self.items()[i].venue.lat, "lng": self.items()[i].venue.lng};
       var title = self.items()[i].venue.displayName;
+
       // Create a marker per location, and put into markers array.
+      var contentString = '<div>This is some data '+ title  + '</div>';
+      var largeInfowindow = new google.maps.InfoWindow({
+      });
+
+//same issue as this guy:
+//https://stackoverflow.com/questions/19784953/google-map-api-infowindow-in-foreach-loop
+//https://stackoverflow.com/questions/32798480/assign-infowindow-for-each-marker-in-google-maps
+
       var marker = new google.maps.Marker({
         map: map,
         position: position,
@@ -39,15 +65,36 @@ var ViewModel = function() {
         animation: google.maps.Animation.DROP,
         id: i
       });
+
+      //same issue as this guy:
+      //https://stackoverflow.com/questions/32798480/assign-infowindow-for-each-marker-in-google-maps
+
+       google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          self.performances(self.items()[i].performance);
+          var contentString = '<h3>' + self.items()[i].venue.displayName + '</h3>';
+          contentString += '<h4>' + self.items()[i].displayName + '</h4>'
+          for (var j = 0; j <  self.performances().length; j++) {
+          contentString += '<div> Artist ' + (j+1) + ': ' + '<a href=' + self.performances()[j].artist.uri + ' target="_Blank">' + self.performances()[j].displayName + '</a></div>';
+        };
+        contentString += '<br><img src="./img/powered-by-songkick-pink.png" width="100">';
+            largeInfowindow.setContent(contentString);
+            largeInfowindow.open(map, marker);
+        }
+    })(marker, i));
+
       // Push the marker to our array of markers.
       markers.push(marker);
+
+
+
       //bounds.extend(markers[i].position);
     }
     // Extend the boundaries of the map for each marker
     //map.fitBounds(bounds);
 
   });
-}
+};
 
 
   self.filter = ko.observable('');
