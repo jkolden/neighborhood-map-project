3dd57305e3fc54c5f1ed16b56737adb19b4948b8
John Kolden <john.kolden@gmail.com>
2018-04-16 18:05:56 -0700

    added code for songkick API

--- js/app.js
+++ js/app.js
@@ -4,23 +4,56 @@ function venue(name) {
   self.name = name;
 }
 
+var map;
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+   map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.7749, lng: -122.4194},
+    zoom: 13
+  });
+};
 
 var ViewModel = function() {
 
   var self = this;
+  self.items = ko.observableArray([]);
+
+  self.loadUserData = function() {
+
+  //good tututorial on loading server-side data here: https://code.tutsplus.com/tutorials/accessing-external-data--net-31306
+  $.getJSON("http://api.songkick.com/api/3.0/metro_areas/26330/calendar.json?apikey=Co6kY8qQPER2gGwi", function(data) {
+    self.items(data.resultsPage.results.event);
+
+    // The following group uses the location array to create an array of markers on initialize.
+    var markers = [];
+
+    for (var i = 0; i < self.items().length; i++) {
+      // Get the position from the location array.
+      var position = {"lat": self.items()[i].venue.lat, "lng": self.items()[i].venue.lng};
+      var title = self.items()[i].venue.displayName;
+      // Create a marker per location, and put into markers array.
+      var marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
+      // Push the marker to our array of markers.
+      markers.push(marker);
+      //bounds.extend(markers[i].position);
+    }
+    // Extend the boundaries of the map for each marker
+    //map.fitBounds(bounds);
+
+  });
+}
+
 
   self.filter = ko.observable('');
-  self.items = ko.observableArray([
-  {"name": "Fillmore", "lat": 37.783926, "lng": -122.433072},
-  {"name": "The Masonic", "lat": 37.791198, "lng": -122.412982},
-  {"name": "SF Jazz", "lat": 37.776395, "lng": -122.421527},
-  {"name": "Davies Hall", "lat": 37.777612, "lng": -122.420623}
 
-    //not sure if we should go this route but each of these DOES need to be selectable so I'm not sure
-    //new venue("Filmore West"),
-    //new venue("The Masonic")
-  ]);
 
+/*
   //I was on the wrong track with this so I used this as a reference:
   //https://stackoverflow.com/questions/31188583/filter-table-contents
   self.filteredItems = ko.computed(function() {
@@ -32,6 +65,8 @@ var ViewModel = function() {
       return i.indexOf(filter) > -1;
     });
   });
+  */
 };
+
 
 ko.applyBindings(new ViewModel());
